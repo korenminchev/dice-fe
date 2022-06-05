9accb9d16d5960426464ebe880d94a02251d3940
Koren Minchev <korenminchev@gmail.com>
2022-06-05 14:30:47 +0300

    Added assets.

--- lib/features/game_rules/game_rules_page.dart
+++ lib/features/game_rules/game_rules_page.dart
@@ -1,5 +1,6 @@
 import 'package:dice_fe/core/widgets/app_bar_title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class GameRulesPage extends StatelessWidget {
   const GameRulesPage({ Key? key }) : super(key: key);
@@ -18,11 +19,23 @@ class GameRulesPage extends StatelessWidget {
           child: Column(
             
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              Text("Game Rules", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700)),
-              SizedBox(height: 24),
-              Text("Dice is a gambling game played using dice (wow how shocking)\n\n"),
-              Text("Setup\n", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+            children: [
+              const Text("Game Rules", style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700)),
+              const SizedBox(height: 24),
+              const Text("Dice is a gambling game played using dice (wow how shocking)\n\n"),
+              const Text("Setup\n", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+              const Text(
+                "All of the players start with the same amount of \n"
+                "dice, decided at the start of the game.",
+                style: TextStyle(fontSize: 16),
+              ),
+              const Text(
+                "Each player can only see his own dice set.",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)
+              ),
+              Image.asset(
+                "assets/images/initial_rules.png",
+              )
             ],
           ),
         )
